9abcd1a919eebb0ee71753f1fbb079174f86c813
Rares Liscan <liscanrares@yahoo.com>
2021-10-20 21:41:37 +0300

    Heroku push is finished

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-dotenv.config();
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
