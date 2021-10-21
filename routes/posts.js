c8182034540cc78206c060d78fd3a4b61bd8af1b
Rares Liscan <liscanrares@yahoo.com>
2021-10-22 00:35:44 +0300

    Authentication is fully functional

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,15 @@ import express, { Router } from 'express';
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
+//Middleware
+import auth from '../middleware/auth.js';
+
 //http://localhost:5000/posts
 const router = express.Router();
 router.get("/", getPosts);
-router.post("/", createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post("/", auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
